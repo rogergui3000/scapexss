dfb61c4874fe1930526bbe0ffc8c9c11860a017c
Mekeidje roger <rogergui3000@gmail.com>
2016-04-13 16:10:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * Scapexss
+ * https://github.com/rogergui3000/scapexss
+ *
+ * Copyright (c) 2016 Meekidje Roger
+ * Licensed under the MIT license.
+ */
+
+var chars = {
+  '&amp;': '&',
+  '&quot;': '"',
+  '&#39;': '\'',
+  '&lt;': '<',
+  '&gt;': '>'
+};
+
+/**
+ * Escape special characters in the given string of html.
+ *
+ * @param  {String} html
+ * @return {String}
+ */
+module.exports = {
+  escape: function(html) {
+    if (!html) {
+      return '';
+    }
+
+    var values = Object.keys(chars).map(function(key) { return chars[key]; });
+    var re = new RegExp('(' + values.join('|') + ')', 'g');
+
+    return String(html).replace(re, function(match) {
+      for (var key in chars) {
+        if (chars.hasOwnProperty(key) && chars[key] === match) {
+          return key;
+        }
+      }
+    });
+  },
+
+  /**
+   * Unescape special characters in the given string of html.
+   *
+   * @param  {String} html
+   * @return {String}
+   */
+  unescape: function(html) {
+    if (!html) {
+      return '';
+    }
+
+    var re = new RegExp('(' + Object.keys(chars).join('|') + ')', 'g');
+
+    return String(html).replace(re, function(match) {
+      return chars[match];
+    });
+  }
+};
